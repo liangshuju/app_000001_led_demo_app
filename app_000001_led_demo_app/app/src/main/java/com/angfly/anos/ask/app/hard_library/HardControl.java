dc0fe32864918c5f7945cd2505fe078f042f26b2
liangshuju <www.liangshuju.com>
2018-11-24 15:58:53 +0800

    v1.0.4 app update clean up.

---
+++ app_000001_led_demo_app/app/src/main/java/com/angfly/anos/ask/app/hard_library/HardControl.java
@@ -0,0 +1,17 @@
+package com.angfly.anos.ask.app.hard_library;
+
+public class HardControl {
+
+    public static native int ledCtrl(int which, int status);
+    public static native int ledOpen();
+    public static native void ledClose();
+
+    static {
+        try {
+            System.loadLibrary("hard_control");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
