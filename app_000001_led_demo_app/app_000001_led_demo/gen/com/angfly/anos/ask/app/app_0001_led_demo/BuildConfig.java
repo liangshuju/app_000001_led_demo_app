cc22d93c7323ccec205f4bf116e858be65171c46
liangshuju <www.liangshuju.com>
2018-12-15 20:55:17 +0800

    v1.0.5 you can use it by studio.

---
+++ app_000001_led_demo_app/app_000001_led_demo/gen/com/angfly/anos/ask/app/app_0001_led_demo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.angfly.anos.ask.app.app_0001_led_demo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- version.h
+++ version.h
@@ -1,1 +1,1 @@
-#define APP_APP_TO_HARDWARE_00001 "v1.0.4"
+#define APP_APP_TO_HARDWARE_00001 "v1.0.5"
